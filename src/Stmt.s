7f33bb9cf44a44b61ed38faf95a45d2cca144407
TolimanStaR <toliman.st4r@gmail.com>
2020-10-30 01:11:39 +0300

    Merge issues

---
+++ src/Stmt.s
@@ -0,0 +1,419 @@
+	.file "/home/toliman/LamaProject/compilers-2020/src/Stmt.lama"
+
+	.stabs "/home/toliman/LamaProject/compilers-2020/src/Stmt.lama",100,0,0,.Ltext
+
+	.globl	LevalStmt
+
+	.globl	initStmt
+
+	.data
+
+string_0:	.string	"Stmt eval not implemented\n"
+
+_init:	.int 0
+
+	.section custom_data,"aw",@progbits
+
+filler:	.fill	0, 4, 1
+
+	.text
+
+.Ltext:
+
+	.stabs "data:t1=r1;0;4294967295;",128,0,0,0
+
+# PUBLIC ("initStmt") / 
+
+# PUBLIC ("LevalStmt") / 
+
+# EXTERN ("LgetOutput") / 
+
+# EXTERN ("LreadWorld") / 
+
+# EXTERN ("LwriteWorld") / 
+
+# EXTERN ("LcreateWorld") / 
+
+# EXTERN ("LevalExpr") / 
+
+# EXTERN ("Li__Infix_6045") / 
+
+# EXTERN ("LemptyState") / 
+
+# EXTERN ("LtagHash") / 
+
+# EXTERN ("LflatCompare") / 
+
+# EXTERN ("LcompareTags") / 
+
+# EXTERN ("LkindOf") / 
+
+# EXTERN ("Ltime") / 
+
+# EXTERN ("Lrandom") / 
+
+# EXTERN ("LdisableGC") / 
+
+# EXTERN ("LenableGC") / 
+
+# EXTERN ("Ls__Infix_37") / 
+
+# EXTERN ("Ls__Infix_47") / 
+
+# EXTERN ("Ls__Infix_42") / 
+
+# EXTERN ("Ls__Infix_45") / 
+
+# EXTERN ("Ls__Infix_43") / 
+
+# EXTERN ("Ls__Infix_62") / 
+
+# EXTERN ("Ls__Infix_6261") / 
+
+# EXTERN ("Ls__Infix_60") / 
+
+# EXTERN ("Ls__Infix_6061") / 
+
+# EXTERN ("Ls__Infix_3361") / 
+
+# EXTERN ("Ls__Infix_6161") / 
+
+# EXTERN ("Ls__Infix_3838") / 
+
+# EXTERN ("Ls__Infix_3333") / 
+
+# EXTERN ("Ls__Infix_58") / 
+
+# EXTERN ("Li__Infix_4343") / 
+
+# EXTERN ("Lcompare") / 
+
+# EXTERN ("Lwrite") / 
+
+# EXTERN ("Lread") / 
+
+# EXTERN ("Lfailure") / 
+
+# EXTERN ("Lfwrite") / 
+
+# EXTERN ("Lfread") / 
+
+# EXTERN ("Lfclose") / 
+
+# EXTERN ("Lfopen") / 
+
+# EXTERN ("Lfprintf") / 
+
+# EXTERN ("Lprintf") / 
+
+# EXTERN ("LmakeString") / 
+
+# EXTERN ("Lsprintf") / 
+
+# EXTERN ("LregexpMatch") / 
+
+# EXTERN ("Lregexp") / 
+
+# EXTERN ("Lsubstring") / 
+
+# EXTERN ("LmatchSubString") / 
+
+# EXTERN ("Lstringcat") / 
+
+# EXTERN ("LreadLine") / 
+
+# EXTERN ("Ltl") / 
+
+# EXTERN ("Lhd") / 
+
+# EXTERN ("Lsnd") / 
+
+# EXTERN ("Lfst") / 
+
+# EXTERN ("Lhash") / 
+
+# EXTERN ("Lclone") / 
+
+# EXTERN ("LmakeArray") / 
+
+# EXTERN ("LstringInt") / 
+
+# EXTERN ("global_sysargs") / 
+
+# EXTERN ("Lsystem") / 
+
+# EXTERN ("LgetEnv") / 
+
+# LABEL ("initStmt") / 
+
+initStmt:
+
+# BEGIN ("initStmt", 0, 0, [], [], []) / 
+
+	.type initStmt, @function
+
+	.stabs "initStmt:F1",36,0,0,initStmt
+
+	.cfi_startproc
+
+	.cfi_adjust_cfa_offset	4
+
+	movl	_init,	%eax
+	test	%eax,	%eax
+	jz	_continue
+	ret
+_continue:
+
+	movl	$1,	_init
+	pushl	%ebp
+	.cfi_adjust_cfa_offset	4
+
+	movl	%esp,	%ebp
+	.cfi_def_cfa_register	5
+
+	subl	$LinitStmt_SIZE,	%esp
+	movl	%esp,	%edi
+	movl	$filler,	%esi
+	movl	$LSinitStmt_SIZE,	%ecx
+	rep movsl	
+	call	initState
+	call	initExpr
+	call	initWorld
+# SLABEL ("L1") / 
+
+L1:
+
+# CONST (0) / 
+
+	movl	$1,	%ebx
+# SLABEL ("L2") / 
+
+L2:
+
+# END / 
+
+	movl	%ebx,	%eax
+LinitStmt_epilogue:
+
+	movl	%ebp,	%esp
+	popl	%ebp
+	.cfi_restore	5
+
+	.cfi_def_cfa	4, 4
+
+	ret
+	.cfi_endproc
+
+	.set	LinitStmt_SIZE,	0
+
+	.set	LSinitStmt_SIZE,	0
+
+	.size initStmt, .-initStmt
+
+# LABEL ("LevalStmt") / 
+
+LevalStmt:
+
+# BEGIN ("LevalStmt", 2, 0, [], ["input"; "stmt"], [{ blab="L4"; elab="L5"; names=[]; subs=[{ blab="L7"; elab="L8"; names=[]; subs=[]; }]; }]) / 
+
+	.type evalStmt, @function
+
+	.stabs "evalStmt:F1",36,0,0,LevalStmt
+
+	.stabs "input:p1",160,0,0,8
+
+	.stabs "stmt:p1",160,0,0,12
+
+	.cfi_startproc
+
+	.cfi_adjust_cfa_offset	4
+
+	pushl	%ebp
+	.cfi_adjust_cfa_offset	4
+
+	movl	%esp,	%ebp
+	.cfi_def_cfa_register	5
+
+	subl	$LLevalStmt_SIZE,	%esp
+	movl	%esp,	%edi
+	movl	$filler,	%esi
+	movl	$LSLevalStmt_SIZE,	%ecx
+	rep movsl	
+# SLABEL ("L4") / 
+
+L4:
+
+# SLABEL ("L7") / 
+
+L7:
+
+# LINE (25) / 
+
+	.stabn 68,0,25,0
+
+	.stabn 68,0,25,.L0-LevalStmt
+
+.L0:
+
+# CLOSURE ("LemptyState", []) / 
+
+	pushl	$LemptyState
+	pushl	$1
+	call	Bclosure
+	addl	$8,	%esp
+	movl	%eax,	%ebx
+# LD (Arg (0)) / 
+
+	movl	8(%ebp),	%ecx
+# CALL ("LcreateWorld", 1, false) / 
+
+	pushl	%ebx
+	pushl	%ecx
+	call	LcreateWorld
+	addl	$4,	%esp
+	popl	%ebx
+	movl	%eax,	%ecx
+# CALL (".array", 2, false) / 
+
+	pushl	%ecx
+	pushl	%ebx
+	pushl	$5
+	call	Barray
+	addl	$12,	%esp
+	movl	%eax,	%ebx
+# LD (Arg (1)) / 
+
+	movl	12(%ebp),	%ecx
+# CALL ("Leval", 2, false) / 
+
+	pushl	%ecx
+	pushl	%ebx
+	call	Leval
+	addl	$8,	%esp
+	movl	%eax,	%ebx
+# CALL ("Lsnd", 1, false) / 
+
+	pushl	%ebx
+	call	Lsnd
+	addl	$4,	%esp
+	movl	%eax,	%ebx
+# CALL ("LgetOutput", 1, true) / 
+
+	pushl	%ebx
+	call	LgetOutput
+	addl	$4,	%esp
+	movl	%eax,	%ebx
+# SLABEL ("L8") / 
+
+L8:
+
+# SLABEL ("L5") / 
+
+L5:
+
+# END / 
+
+	movl	%ebx,	%eax
+LLevalStmt_epilogue:
+
+	movl	%ebp,	%esp
+	popl	%ebp
+	.cfi_restore	5
+
+	.cfi_def_cfa	4, 4
+
+	ret
+	.cfi_endproc
+
+	.set	LLevalStmt_SIZE,	0
+
+	.set	LSLevalStmt_SIZE,	0
+
+	.size LevalStmt, .-LevalStmt
+
+# LABEL ("Leval") / 
+
+Leval:
+
+# BEGIN ("Leval", 2, 0, [], ["c"; "stmt"], [{ blab="L16"; elab="L17"; names=[]; subs=[{ blab="L19"; elab="L20"; names=[]; subs=[]; }]; }]) / 
+
+	.type eval, @function
+
+	.stabs "eval:F1",36,0,0,Leval
+
+	.stabs "c:p1",160,0,0,8
+
+	.stabs "stmt:p1",160,0,0,12
+
+	.cfi_startproc
+
+	.cfi_adjust_cfa_offset	4
+
+	pushl	%ebp
+	.cfi_adjust_cfa_offset	4
+
+	movl	%esp,	%ebp
+	.cfi_def_cfa_register	5
+
+	subl	$LLeval_SIZE,	%esp
+	movl	%esp,	%edi
+	movl	$filler,	%esi
+	movl	$LSLeval_SIZE,	%ecx
+	rep movsl	
+# SLABEL ("L16") / 
+
+L16:
+
+# SLABEL ("L19") / 
+
+L19:
+
+# LINE (20) / 
+
+	.stabn 68,0,20,0
+
+	.stabn 68,0,20,.L1-Leval
+
+.L1:
+
+# STRING ("Stmt eval not implemented\\n") / 
+
+	movl	$string_0,	%ebx
+	pushl	%ebx
+	call	Bstring
+	addl	$4,	%esp
+	movl	%eax,	%ebx
+# CALL ("Lfailure", 1, true) / 
+
+	pushl	%ebx
+	call	Lfailure
+	addl	$4,	%esp
+	movl	%eax,	%ebx
+# SLABEL ("L20") / 
+
+L20:
+
+# SLABEL ("L17") / 
+
+L17:
+
+# END / 
+
+	movl	%ebx,	%eax
+LLeval_epilogue:
+
+	movl	%ebp,	%esp
+	popl	%ebp
+	.cfi_restore	5
+
+	.cfi_def_cfa	4, 4
+
+	ret
+	.cfi_endproc
+
+	.set	LLeval_SIZE,	0
+
+	.set	LSLeval_SIZE,	0
+
+	.size Leval, .-Leval
+
