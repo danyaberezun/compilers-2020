ddc127f373ad63a9a5fa70f473c5c4c96a77500e
Nikita Tsarev <nikts53@gmail.com>
2021-01-25 21:37:26 +0300

    something's wacky

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -2,6 +2,7 @@
 # include <stdlib.h>
 # include <stdarg.h>
 # include <string.h>
+# include "vector.h"
 
 # define UNBOXED(x)  (((int) (x)) &  0x0001)
 # define UNBOX(x)    (((int) (x)) >> 1)
@@ -9,6 +10,7 @@
 
 # define STRING_TAG  0x00000001
 # define ARRAY_TAG   0x00000003
+# define SEXP_TAG    0x00000005
 
 # define LEN(x) ((x & 0xFFFFFFF8) >> 3)
 # define TAG(x) (x & 0x00000007)
@@ -34,6 +36,27 @@ void* Barray (int n0, ...) {
   r = (data*) malloc (sizeof(int) * (n+1));
 
   r->tag = ARRAY_TAG | (n << 3);
+
+  va_start(args, n0);
+
+  for (i = 0; i<n; i++) {
+    ai = va_arg(args, int);
+    ((int*) r->contents)[i] = ai;
+  }
+  va_end(args);
+
+  return r->contents;
+}
+
+void* Bsexp (int n0, ...) {
+  int     n = UNBOX(n0);
+  va_list args;
+  int     i, ai;
+  data    *r;
+
+  r = (data*) malloc (sizeof(int) * (n+1));
+
+  r->tag = SEXP_TAG | (n << 3);
 
   va_start(args, n0);
 
@@ -77,11 +100,7 @@ void* Bsta (int i0, void *v, void *x) {
   return v;
 }
 
-void Lwrite (int x) {
-  printf ("%d\n", UNBOX (x));
-}
-
-int Lread () {
+int Bread () {
   int result;
 
   scanf  ("%d", &result);
@@ -89,3 +108,60 @@ int Lread () {
   return BOX(result);
 }
 
+void Bmatchfailure() {
+  fprintf(stderr, "Match failure!\n");
+  exit(EXIT_FAILURE);
+}
+
+int Bstrcmp(char *a, char *b) {
+  return BOX(strcmp(a, b) == 0);
+}
+
+void to_string(char *buf, void *p) {
+  if (UNBOXED(p)) {
+    string_printf(buf, "%d", UNBOX(p));
+    return;
+  }
+
+  data *a = TO_DATA(p);
+  if (TAG(a->tag) == STRING_TAG) {
+    string_printf(buf, "\"%s\"", a->contents);
+  } else if (TAG(a->tag) == ARRAY_TAG) {
+    string_append(buf, "[");
+    for (int i = 0; i < LEN(a->tag); ++i) {
+      if (i > 0) {
+        string_append(buf, ", ");
+      }
+      to_string(buf, (void*)(((int*)a->contents)[i]));
+    }
+    string_append(buf, "]");
+  } else if (TAG(a->tag) == SEXP_TAG) {
+    char *tag = (char*)(((int*)a->contents)[0]);
+    string_append(buf, tag);
+    if (LEN(a->tag) > 1) {
+      string_append(buf, " (");
+      for (int i = 1; i < LEN(a->tag); ++i) {
+        if (i > 1) {
+          string_append(buf, ", ");
+        }
+        to_string(buf, (void*)(((int*)a->contents)[i]));
+      }
+      string_append(buf, ")");
+    }
+  }
+}
+
+void *Bstringval(void *p) {
+  char *buf = string_new();
+  to_string(buf, p);
+  void *result = Bstring(buf);
+  vector_free(buf);
+  return result;
+}
+
+void Bwrite (void *p) {
+  char *buf = string_new();
+  to_string(buf, p);
+  printf ("%s\n", buf);
+  vector_free(buf);
+}
